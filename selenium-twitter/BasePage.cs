778700564274db0d515f894d1b4108cb2e7c7675
Nathan Isom <nathanisom27@gmail.com>
2015-10-30 12:14:00 -0700

    Initial commit.

---
+++ selenium-twitter/BasePage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium_twitter
+{
+    /// <summary>
+    /// Base page class for page objects
+    /// </summary>
+    class BasePage
+    {
+    }
+}
